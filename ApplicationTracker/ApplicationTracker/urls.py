2184a823205c0f6666b4daafc4a9877d5942cc3a
Sam Weber <sweber102@gmail.com>
2019-04-26 22:30:49 -0700

    Created accounts app

--- ApplicationTracker/ApplicationTracker/urls.py
+++ ApplicationTracker/ApplicationTracker/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pages.urls')),
     path('applications/', include('applications.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]

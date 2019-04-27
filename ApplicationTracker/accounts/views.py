74bd058ac058f6987a2b72385137a6fd91bf74f0
Sam Weber <sweber102@gmail.com>
2019-04-27 12:03:23 -0700

    User authentication

--- ApplicationTracker/accounts/views.py
+++ ApplicationTracker/accounts/views.py
@@ -1,18 +1,51 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        # Register User
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        first_name = request.POST['first']
+        last_name = request.POST['last']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Please choose a different username')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already in use')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'Account created, please log in')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
-        # Login User
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
+
     else:
         return render(request, 'accounts/login.html')
 
